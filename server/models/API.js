3a911fa839f05d4297f3927af53d3b6c7caddcb3
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-02-19 20:17:49 -0500

    model comment updates

--- server/models/API.js
+++ server/models/API.js
@@ -15,81 +15,20 @@ const ObjectId = Schema.Types.ObjectId;
     "api_slug" : "www.example.com/user_management_api",
     "user" : ObjectId("60d21b8a3c4a7e49b1b28767"),
     "mongo_uri" : "mongodb://localhost:27017",
-    "mongo_schema" : '
-    const APISchema = new Schema({
-    //api name/title
-    name: {
-        required: true,
-        type: String
-
+    "mongo_schema" : {
+        "name" : "String",
+        "email" : "String",
+        "password" : "String"
     },
-
-    //api description
-    description: {
-        required: true,
-        type: String
-    },
-    api_keys: [
-        {
-            type: String
-        }
-    ],
-
-    //api slug such as www.example.com/api_slug/endpoint_slug
-    api_slug:{
-        required: true,
-
-    },
-
-    //api owner
-    user : {
-        type: ObjectId,
-        ref: "user"
-    },
-    //mongo_uri for the database
-    mongo_uri: {
-        required:true,
-        type:String
-    },
-
-    //mongo schema for the database
-    mongo_schema: {
-        required:true,
-        type:String
-    },
-
-    //database related to the api
-    database_name: {
-        type: ObjectId,
-        ref: 'Database'
-    },
-
-    //endpoints related to the api
-    endpoints: [
-        {
-            type :ObjectId,
-            ref: 'Endpoint'
-        }
-    ],
-
-    //created at
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
-   
-});
-
-module.exports = mongoose.model('API', APISchema);
-    ',
-    "database_name" : ObjectId("60d21b8a3c4a7e49b1b28759"),
+    "database_name" : ObjectId("60d21b8a3c4a7e49b1b28767"),
     "endpoints" : [
-        ObjectId("60d21b8a3c4a7e49b1b28760"),
-        ObjectId("60d21b8a3c4a7e49b1b28761"),
-        ObjectId("60d21b8a3c4a7e49b1b28762")
+        ObjectId("60d21b8a3c4a7e49b1b28767"),
+        ObjectId("60d21b8a3c4a7e49b1b28767")
     ],
-    "created_at" : ISODate("2021-06-22T01:22:34.639Z")
+    "created_at" : ISODate("2021-06-23T19:41:14.000Z")
 }
+
+    
 */
 
 const APISchema = new Schema({
@@ -128,10 +67,9 @@ const APISchema = new Schema({
         type:String
     },
 
-    //mongo schema for the database
+    //mongo schema for the database defined by a hashmap of key-value pairs of name : type
     mongo_schema: {
-        required:true,
-        type:String
+       type:Object
     },
 
     //database related to the api
--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,17 @@ import bcrypt from 'bcryptjs';
 
 const Schema = mongoose.Schema;
 
+/* example of a user schema
+{
+    email: "
+    password: "
+    username: "
+    reset_password_token: "
+    profile_picture: "
+    apis: [ObjectId("60d21b8a3c4a7e49b1b28767"), ObjectId("60d21b8a3c4a7e49b1b28767")]
+}
+*/
+
 const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String,
